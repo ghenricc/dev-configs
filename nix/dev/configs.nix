021e34fdc60a31356707cc44882e982a8d2f7e6e
Carson Henrich <carson03henrich@gmail.com>
2025-01-18 15:31:18 -0800

    chore(conform): expand available scopes
    Adds additional scopes to the accepted scopes to better account for the
    commits being made in this repo.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Changes to be committed:
    #	modified:   nix/dev/configs.nix
    #

--- nix/dev/configs.nix
+++ nix/dev/configs.nix
@@ -7,35 +7,12 @@
 ,
 }:
 let
+  inherit (inputs.std) dmerge;
   inherit (inputs.std.lib.dev) mkNixago;
   inherit (inputs.cells.src) cfg;
   repository = "dev-configs";
 in
 {
-
-  # Base Config: ../src/cfg/lefthook.nix 
-  # Config Reference: https://evilmartians.github.io/lefthook/configuration/index.html
-  lefthook = mkNixago cfg.lefthook {
-    # FIXME I didn't want to make a bypass here but need to because nil has issues with scopedImport https://github.com/oxalica/nil/issues/72 
-    data.pre-commit.commands.nil.exclude = "^nix/src/cfg/.*\.nix";
-  };
-
-
-  # Base Config: ../src/cfg/conform.nix 
-  # Config Reference: https://github.com/siderolabs/conform?tab=readme-ov-file
-  conform = mkNixago cfg.conform;
-
-  # Base Config: ../src/cfg/editorconfig.nix 
-  # Config Reference: https://spec.editorconfig.org/
-  editorconfig = mkNixago cfg.editorconfig;
-
-  # Base Config: ../src/cfg/treefmt.nix 
-  # Config Reference: https://treefmt.com/latest/getting-started/configure/#ci 
-  treefmt = mkNixago cfg.treefmt;
-
-  # Base Config: ../src/cfg/typos.nix 
-  # Config Reference: https://github.com/crate-ci/typos/blob/master/docs/reference.md
-  typos = mkNixago cfg.typos;
 
   # Base Config: ../src/cfg/cog.nix 
   # Config Reference: https://docs.cocogitto.io/reference/config.html
@@ -53,17 +30,50 @@ in
     };
   };
 
+
+  # Base Config: ../src/cfg/conform.nix 
+  # Config Reference: https://github.com/siderolabs/conform?tab=readme-ov-file
+  conform = mkNixago cfg.conform {
+    data.commit.conventional.scopes = dmerge.append [
+      "cog"
+      "conform"
+      "editorconfig"
+      "githubsettings"
+      "lefthook"
+      "mdbook"
+      "treefmt"
+      "typos"
+    ];
+  };
+
+  # Base Config: ../src/cfg/editorconfig.nix 
+  # Config Reference: https://spec.editorconfig.org/
+  editorconfig = mkNixago cfg.editorconfig;
+
   # Base Config: ../src/cfg/githubsettings.nix 
   # Config Reference: https://github.com/github/safe-settings/blob/main-enterprise/docs/sample-settings/settings.yml
-  githubsettings = mkNixago
-    cfg.githubsettings
-    {
-      data.repository = {
-        name = repository;
-        inherit (import (inputs.self + /flake.nix)) description;
-        homepage = "";
-        topics = "nix nix-flakes devshells";
-        private = false;
-      };
+  githubsettings = mkNixago cfg.githubsettings {
+    data.repository = {
+      name = repository;
+      inherit (import (inputs.self + /flake.nix)) description;
+      homepage = "";
+      topics = "nix nix-flakes devshells";
+      private = false;
     };
+  };
+
+  # Base Config: ../src/cfg/lefthook.nix 
+  # Config Reference: https://evilmartians.github.io/lefthook/configuration/index.html
+  lefthook = mkNixago cfg.lefthook {
+    # FIXME I didn't want to make a bypass here but need to because nil has issues with scopedImport https://github.com/oxalica/nil/issues/72 
+    data.pre-commit.commands.nil.exclude = "^nix/src/cfg/.*\.nix";
+  };
+
+  # Base Config: ../src/cfg/treefmt.nix 
+  # Config Reference: https://treefmt.com/latest/getting-started/configure/#ci 
+  treefmt = mkNixago cfg.treefmt;
+
+  # Base Config: ../src/cfg/typos.nix 
+  # Config Reference: https://github.com/crate-ci/typos/blob/master/docs/reference.md
+  typos = mkNixago cfg.typos;
 }
